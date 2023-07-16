c32fef54463582cc1c423f3781f4b1f9ddf90672
eko5624 <562433403@qq.com>
2023-07-16 18:12:59 +0800

    Update target-gcc-with-mcf.sh

--- target-gcc-with-mcf.sh
+++ target-gcc-with-mcf.sh
@@ -300,7 +300,6 @@ $M_SOURCE/gcc-13.1.0/configure \
   --disable-symvers \
   --disable-libstdcxx-pch \
   --disable-libstdcxx-debug \
-  --disable-libstdcxx-backtrace \
   --disable-win32-registry \
   --disable-version-specific-runtime-libs \
   --disable-sjlj-exceptions \
