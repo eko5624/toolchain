808480d7e4c8977501d08262d2ab97e26a28fd96
eko5624 <562433403@qq.com>
2023-07-18 22:46:31 +0800

    Update and rename target-gcc-with-mcf-no-mingw.sh to target-gcc-with-mcf.sh

--- target-gcc-with-mcf.sh
+++ target-gcc-with-mcf.sh
@@ -374,6 +374,8 @@ done
 cp $M_TARGET/lib/libgcc_s_seh-1.dll $M_TARGET/bin/
 cp $M_TARGET/bin/gcc.exe $M_TARGET/bin/cc.exe
 cp $M_TARGET/bin/$MINGW_TRIPLE-gcc.exe $M_TARGET/bin/$MINGW_TRIPLE-cc.exe
+find $M_TARGET/lib -type f \( -name "*.dll.a" ! -name "libmcfgthread.dll.a" \) -print0 | xargs -0 -I {} rm {}
+find $M_TARGET/lib -type f -name "*.la" -print0 | xargs -0 -I {} rm {}
 
 echo "building make"
 echo "======================="
