4cdffec95bb4bb6a628f90a081f4d8ed9061d7ba
eko5624 <562433403@qq.com>
2023-07-21 21:09:38 +0800

    Update toolchain.cmake

--- toolchain.cmake
+++ toolchain.cmake
@@ -1,14 +1,11 @@
 SET(CMAKE_SYSTEM_NAME Windows)
 SET(CMAKE_SYSTEM_PROCESSOR x86_64)
 
-SET(CROSS $ENV{M_CROSS})
-SET(TOP $ENV{TOP_DIR})
-
-SET(CMAKE_C_COMPILER /__w/toolchain-test/toolchain-test/cross/bin/x86_64-w64-mingw32-gcc)
-SET(CMAKE_CXX_COMPILER /__w/toolchain-test/toolchain-test/cross/bin/x86_64-w64-mingw32-g++)
-SET(CMAKE_RC_COMPILER /__w/toolchain-test/toolchain-test/cross/bin/x86_64-w64-mingw32-windres)
-SET(CMAKE_ASM_COMPILER /__w/toolchain-test/toolchain-test/cross/bin/x86_64-w64-mingw32-as)
-SET(CMAKE_RANLIB /__w/toolchain-test/toolchain-test/cross/bin/x86_64-w64-mingw32-ranlib)
+SET(CMAKE_C_COMPILER x86_64-w64-mingw32-gcc)
+SET(CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++)
+SET(CMAKE_RC_COMPILER n/x86_64-w64-mingw32-windres)
+SET(CMAKE_ASM_COMPILER x86_64-w64-mingw32-as)
+SET(CMAKE_RANLIB x86_64-w64-mingw32-ranlib)
 
 SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
