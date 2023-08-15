716a6c0f289df8c3a360868273166ab68c757335
eko5624 <562433403@qq.com>
2023-08-15 15:28:27 +0800

    Update cross-gcc-mcf.sh

--- cross-gcc-mcf.sh
+++ cross-gcc-mcf.sh
@@ -173,7 +173,7 @@ make install
 cd $M_CROSS
 find $MINGW_TRIPLE/lib -type f -name "*.la" -print0 | xargs -0 -I {} rm {}
 find $MINGW_TRIPLE/lib -type f \( -name "*.dll.a" ! -name "libmcfgthread.dll.a" \) -print0 | xargs -0 -I {} rm {}
-mv $MINGW_TRIPLE/bin/libmcfgthread-1.dll cross/bin
+mv $MINGW_TRIPLE/bin/libmcfgthread-1.dll bin
 rm -f mingw
 rm -rf share
 echo "$VER_GCC" > $M_CROSS/version.txt
