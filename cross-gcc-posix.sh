1881144adb8cd42942a5fcc48c0e55383d031d47
eko5624 <562433403@qq.com>
2023-08-31 19:47:59 +0800

    Update cross-gcc-posix.sh

--- cross-gcc-posix.sh
+++ cross-gcc-posix.sh
@@ -23,12 +23,20 @@ export PATH="$M_CROSS/bin:$PATH"
 mkdir -p $M_SOURCE
 mkdir -p $M_BUILD
 
+echo "gettiong json ver"
+echo "======================="
+json_ver=$(curl -s "https://raw.githubusercontent.com/eko5624/nginx-nosni/master/old.json")
+declare -A ver_array
+while IFS="=" read -r key value; do
+    ver_array[$key]=$value
+done < <(echo "$json_ver" | jq -r 'to_entries | map("\(.key)=\(.value|tostring)") | .[]')
+
 echo "gettiong source"
 echo "======================="
 cd $M_SOURCE
 
-VER_BINUTILS=2.41
-VER_GCC=13.2.0
+VER_BINUTILS=${ver[binutils]}
+VER_GCC=${ver[GCC]}
 
 #binutils
 wget -c -O binutils-$VER_BINUTILS.tar.bz2 http://ftp.gnu.org/gnu/binutils/binutils-$VER_BINUTILS.tar.bz2
