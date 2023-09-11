cebaf89e2a8e5dd556aa03ef824fd1146ae4c1f5
eko5624 <562433403@qq.com>
2023-09-11 09:42:55 +0800

    Create ver.sh

---
+++ ver.sh
@@ -0,0 +1,8 @@
+VER_BINUTILS=${ver[binutils]}
+VER_GCC=${ver[GCC]}
+VER_GMP=${ver[gmp]}
+VER_MPFR=${ver[mpfr]}
+VER_MPC=${ver[mpc]}
+VER_ISL=${ver[isl]}
+VER_MAKE=${ver[make]}
+VER_PKGCONF=${ver[pkgconf]}
