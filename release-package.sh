17298727333c6b3d8b0d8333a776c3ec188d2b01
eko5624 <562433403@qq.com>
2023-10-05 08:01:04 +0800

    Update release-package.sh

--- release-package.sh
+++ release-package.sh
@@ -1,19 +1,6 @@
 #!/bin/bash
 set -ex
-CURL_RETRIES="--connect-timeout 60 --retry 5 --retry-delay 5"
-
-# Delete assets
-asset_id=($(curl -u $GITHUB_ACTOR:$GH_TOKEN $CURL_RETRIES \
-  -H "Accept: application/vnd.github.v3+json" \
-  https://api.github.com/repos/${GITHUB_REPOSITORY}/releases/tags/toolchain \
-| jq -r '.assets[] | select(.name | startswith("'"$1"'")) | .id' | tr -d '\r'))
-
-for id in "${asset_id[@]}"; do
-  curl -u $GITHUB_ACTOR:$GH_TOKEN $CURL_RETRIES \
-    -X DELETE \
-    -H "Accept: application/vnd.github.v3+json" \
-    https://api.github.com/repos/${GITHUB_REPOSITORY}/releases/assets/$id;
-done    
+CURL_RETRIES="--connect-timeout 60 --retry 5 --retry-delay
 
 # Release assets
 curl -u $GITHUB_ACTOR:$GH_TOKEN $CURL_RETRIES \
