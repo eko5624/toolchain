965c9ae752c73d997d27297a9d68a17abe6ea771
eko5624 <562433403@qq.com>
2024-05-15 20:30:30 +0800

    target-gcc-win32.sh: remove '-pthread'

--- target-gcc-win32.sh
+++ target-gcc-win32.sh
@@ -288,7 +288,7 @@ $M_SOURCE/gcc/configure \
   --with-pkgversion="GCC with win32 thread model" \
   CFLAGS='-O2' \
   CXXFLAGS='-O2' \
-  LDFLAGS='-pthread -Wl,--no-insert-timestamp -Wl,--dynamicbase -Wl,--high-entropy-va -Wl,--nxcompat -Wl,--tsaware'
+  LDFLAGS='-Wl,--no-insert-timestamp -Wl,--dynamicbase -Wl,--high-entropy-va -Wl,--nxcompat -Wl,--tsaware'
 make -j$MJOBS
 make install
 
