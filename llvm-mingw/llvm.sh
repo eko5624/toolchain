53de672064c858c374630ef29f3b2182ec658394
eko5624 <562433403@qq.com>
2025-07-25 14:56:52 +0800

    -

--- llvm-mingw/llvm.sh
+++ llvm-mingw/llvm.sh
@@ -14,7 +14,7 @@ MJOBS=$(grep -c processor /proc/cpuinfo)
 M_ROOT=$(pwd)
 M_SOURCE=$M_ROOT/source
 M_CROSS=$M_ROOT/cross
-PATH="$M_CROSS/bin:$PATH"
+PATH="$M_CROSS/bin:/usr/local/fuchsia-clang/bin:$PATH"
 
 unset HOST
 BUILDDIR="build"
